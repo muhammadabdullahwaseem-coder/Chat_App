72b15b02f979c83b23833ef1ccf723ac30f23bc5
Muhammad Abdullah <muhammadabdullah121254@gmail.com>
2026-01-29 22:21:37 +0500

    updated server.js

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,9 @@ import http from 'http'
 
 const app = express();
 const server = http.createServer(app);
+    app.get("/", (req, res) => {
+  res.send("<h1>Server is Running Successfully! </h1>");
+});
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -16,9 +19,7 @@ io.on("connection",(socket)=>{console.log(socket.id)
         socket.join(data);
         console.log(`User ID :- ${socket.id} joined room : ${data}`)
     })
-    app.get("/", (req, res) => {
-  res.send("<h1>Server is Running Successfully! </h1>");
-});
+
 
     socket.on("send_message",(data)=>{console.log("send message data ",data)
     socket.to(data.room).emit("receive_message",data)
